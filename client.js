7917ffa7fa81fdf3cc05c52aefdd1acd133244db
roop <rplawn@icloud.com>
2022-04-21 21:01:57 -0700

    Added event handler and name initials

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require("net");
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write('Name: RGP');
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,21 +1,6 @@
-const net = require("net");
+const connect = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: 50541 // PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
